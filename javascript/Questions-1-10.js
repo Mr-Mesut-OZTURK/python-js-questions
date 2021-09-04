b149bd00102610fc6426f829134c77b956149928
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-09-04 21:38:01 +0300

    added a js questions

--- javascript/Questions-1-10.js
+++ javascript/Questions-1-10.js
@@ -190,7 +190,32 @@ function duplicateEncode(word) {
 
 
 // QUESTİON 7 #######################################################################################
+function flickSwitch(arr) {
+    `
+    Create a function that always returns true 
+    for every item in a given array. However, 
+    if an element is the word "flick", 
+    switch to always returning the opposite boolean value.
 
+    Examples
+
+    flickSwitch(["edabit", "flick", "eda", "bit"]) ➞ [true, false, false, false]
+    flickSwitch(["flick", 11037, 3.14, 53]) ➞ [false, false, false, false]
+    flickSwitch([false, false, "flick", "sheep", "flick"]) ➞ [true, true, false, false, true]
+    
+    Notes
+
+    "flick" will always be given in lowercase.
+    An array may contain multiple flicks.
+    Switch the boolean value on the same element as the flick itself.
+    `
+	num = 0
+	return arr.map(i =>{
+		if (i == "flick") {	num++ }
+		return ((num % 2) == 0 ? true : false)
+	})
+}
+console.log(flickSwitch(["edabit", "flick", "eda", "bit"]))
 
 
 // QUESTİON 8 #######################################################################################
